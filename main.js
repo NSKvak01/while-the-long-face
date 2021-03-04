df339d8aa3f72d9b3e944630379763d2bc743b24
Nastya Kvak <nastya.kvak@codeimmersives.com>
2021-03-04 14:29:26 -0500

    Adds functions

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 function numberDoubler(num) {
     // Your code here
+    while (num <100){
+        num = num*2
+    }
+    return num
 }
 
 function stringRepeater(str) {
     // Your code here
+    let str1 = ''
+    while (str1.length<10){
+        str1= str1+str
+        console.log(str1)
+    }
+    return str1
 }
 
 function makeDivisible(x, y) {
     // Your code here
+    while (!(x%y === 0)){
+        x++
+    }
+    return x;
 }
 
 module.exports = {
